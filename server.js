4a597a47cbbcfa8fd3e13d2ff5b3265494b0c29e
carter00 <piyushdeep00@gmail.com>
2018-02-27 01:34:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('this the sample of article two and it contain svarious text');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('this the sample of article three and it contain svarious text');
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
